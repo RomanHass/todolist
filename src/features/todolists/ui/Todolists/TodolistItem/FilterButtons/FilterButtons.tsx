3d63112027e4f301dd48b94f3848415d31bad6fb
Roman <roman.khassazhirov@gmail.com>
2025-05-21 10:11:17 +0300

    fix: fix FilterButtons

--- src/features/todolists/ui/Todolists/TodolistItem/FilterButtons/FilterButtons.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/FilterButtons/FilterButtons.tsx
@@ -1,14 +1,14 @@
 import { Box } from '@mui/material';
 import { Todolist } from '@/features/todolists/model/todolists-reducer.ts';
 import { containerSx } from '@/common/styles/container.style.ts'
-import { FilterButton } from '@/common/components/FilterButton.tsx'
+import { FilterButton } from './FilterButton/FilterButton.tsx'
 import type { FilterValues } from '@/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx'
+
+const buttons: FilterValues[] = ['all', 'active', 'completed']
 
 export const FilterButtons = ({ todolist }: Props) => {
 
   const { id, filter } = todolist
-
-  const buttons: FilterValues[] = ['all', 'active', 'completed']
 
   return (
     <Box sx={containerSx}>
